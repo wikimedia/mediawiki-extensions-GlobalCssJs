beb6009a1418bde929678f305569aa59d570f1c1
Kunal Mehta <legoktm@gmail.com>
2013-11-11 22:40:49 -0800

    Fix a few messages
    * Removed quotes around "central" in extension description
    * Un-title cased "Global User", JS -> JavaScript
    
    Change-Id: I848376f00da13c56f1f57efac22c54fb18058800

--- GlobalCssJs.i18n.php
+++ GlobalCssJs.i18n.php
@@ -11,8 +11,8 @@ $messages = array();
  * @author Ryan Schmidt
  */
 $messages['en'] = array(
-	'globalcssjs-desc' => 'Allows global CSS and JS on a "central" wiki to be loaded for all wikis in the farm',
-	'tog-enableglobalcssjs' => 'Enable Global User CSS and JS',
+	'globalcssjs-desc' => 'Allows global CSS and JS on a central wiki to be loaded for all wikis in the farm',
+	'tog-enableglobalcssjs' => 'Enable global user CSS and JavaScript',
 );
 
 /** Message documentation
