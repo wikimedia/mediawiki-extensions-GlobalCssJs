940e743e7efd95654921c4612b7c6efbef1f62ba
Kunal Mehta <legoktm@gmail.com>
2015-05-04 18:35:28 -0700

    Empty PHP entry point
    Change-Id: I437f9bb74aac1dda2128b11cb5a981137d99bebe

--- GlobalCssJs.php
+++ GlobalCssJs.php
@@ -1,69 +1,14 @@
 <?php
 
-if( !defined( 'MEDIAWIKI' ) ) {
-	echo( "This is an extension to the MediaWiki software and cannot be used standalone" );
-	die;
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'GlobalCssJs' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['GlobalCssJs'] = __DIR__ . '/i18n/core';
+	/* wfWarn(
+		'Deprecated PHP entry point used for GlobalCssJs extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return true;
+} else {
+	die( 'This version of the GlobalCssJs extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['other'][] = array(
-	'name' => 'GlobalCssJs',
-	'namemsg' => 'globalcssjs-name',
-	'path' => __FILE__,
-	'author' => array( 'Ryan Schmidt', 'Szymon Świerkosz', 'Kunal Mehta' ),
-	'version' => '3.3.0',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:GlobalCssJs',
-	'descriptionmsg' => 'globalcssjs-desc',
-	'license-name' => 'GPL-2.0+',
-);
-
-/**
- * If set, users can put their custom JS and CSS code on pages
- * User:Name/global.js and User:Name/global.css on a central wiki.
- *
- * Administrators can put global code on MediaWiki:Global.js and .css
- *
- * Required properties:
- *   'wiki' - name of the central wiki database
- *   'source' - name of a ResourceLoader source pointing to the central wiki
- *
- * For example:
- * $wgGlobalCssJsConfig = array(
- *   'wiki' => 'metawiki',
- *   'source' => 'metawiki',
- * );
- * $wgResourceLoaderSources['metawiki'] = array(
- *   'apiScript' => '//meta.wikimedia.org/w/api.php',
- *   'loadScript' => '//meta.wikimedia.org/w/load.php',
- * );
- * @var array
- */
-$wgGlobalCssJsConfig = array(
-	'wiki' => false,
-	'source' => false,
-);
-
-/**
- * If true, global site wide css / js is loaded from the central wiki
- * for all users on all page loads.
- * @var boolean
-*/
-$wgUseGlobalSiteCssJs = true;
-
-$wgAutoloadClasses['ResourceLoaderGlobalModule'] = __DIR__ . '/ResourceLoaderGlobalModule.php';
-$wgAutoloadClasses['ResourceLoaderGlobalSiteModule'] = __DIR__ . '/ResourceLoaderGlobalSiteModule.php';
-$wgAutoloadClasses['ResourceLoaderGlobalUserModule'] = __DIR__ . '/ResourceLoaderGlobalUserModule.php';
-$wgAutoloadClasses['GlobalCssJsHooks'] = __DIR__ . '/GlobalCssJs.hooks.php';
-
-// Only for unit tests
-$wgAutoloadClasses['ResourceLoaderGlobalModuleTestCase'] = __DIR__ . '/tests/ResourceLoaderGlobalModuleTestCase.php';
-
-$wgMessagesDirs['GlobalCssJs'] = __DIR__ . '/i18n/core';
-$wgConfigRegistry['globalcssjs'] = 'GlobalVarConfig::newInstance';
-
-$wgExtensionFunctions[] = 'GlobalCssJsHooks::onExtensionFunctions';
-
-$wgHooks['BeforePageDisplay'][] = 'GlobalCssJsHooks::onBeforePageDisplay';
-$wgHooks['ResourceLoaderRegisterModules'][] = 'GlobalCssJsHooks::onResourceLoaderRegisterModules';
-$wgHooks['EditPage::showEditForm:initial'][] = 'GlobalCssJsHooks::onEditPageshowEditForminitial';
-$wgHooks['GetPreferences'][] = 'GlobalCssJsHooks::onGetPreferences';
-$wgHooks['UnitTestsList'][] = 'GlobalCssJsHooks::onUnitTestsList';
