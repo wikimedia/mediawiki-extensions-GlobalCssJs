80e44d18bec8e910a560ae7e5b2c4fc90b687d0f
Timo Tijhof <krinklemail@gmail.com>
2019-07-14 02:34:57 +0100

    tests: Minor clean up in GlobalModuleTest and GlobalSiteModuleTest
    In GlobalSiteModuleTest, move the descriptions to be the data
    set labels and make the expected return value the last parameter
    from the data provider.
    
    Also, let it cover some indirect branches from the constructor
    and other class methods.
    
    Change-Id: I8f89daca99ad80cc052e6801698d600c6b81f47c

--- tests/phpunit/ResourceLoaderGlobalSiteModuleTest.php
+++ tests/phpunit/ResourceLoaderGlobalSiteModuleTest.php
@@ -6,6 +6,9 @@ use MediaWiki\GlobalCssJs\ResourceLoaderGlobalSiteModule;
 use HashConfig;
 use ReflectionMethod;
 
+/**
+ * @covers \MediaWiki\GlobalCssJs\ResourceLoaderGlobalSiteModule
+ */
 class ResourceLoaderGlobalSiteModuleTest extends \PHPUnit\Framework\TestCase {
 	use \MediaWikiCoversValidator;
 	use ResourceLoaderGlobalModuleTestTrait;
@@ -13,101 +16,96 @@ class ResourceLoaderGlobalSiteModuleTest extends \PHPUnit\Framework\TestCase {
 	public static function provideGetPages() {
 		// format: array( $type, array( config => value ), $expectedPages, $skin, $description )
 		return [
-			[
+			'With default settings, 2 CSS global pages are loaded' => [
 				'style',
 				[],
-				[
-					'MediaWiki:Global.css', 'MediaWiki:Global-skinname.css'
-				],
 				'skinname',
-				'With default settings, 2 CSS global pages are loaded'
+				[
+					'MediaWiki:Global.css',
+					'MediaWiki:Global-skinname.css',
+				],
 			],
-			[
+			'With default settings, 2 JS global pages are loaded' => [
 				'script',
 				[],
-				[
-					'MediaWiki:Global.js', 'MediaWiki:Global-skinname.js',
-				],
 				'skinname',
-				'With default settings, 2 JS global pages are loaded'
+				[
+					'MediaWiki:Global.js',
+					'MediaWiki:Global-skinname.js',
+				],
 			],
-			[
+			'No CSS pages are loaded with $wgUseGlobalSiteCssJs = false' => [
 				'style',
 				[ 'UseGlobalSiteCssJs' => false ],
-				[],
 				'skinname',
-				'No CSS pages are loaded with $wgUseGlobalSiteCssJs = false'
+				[],
 			],
-			[
+			'No JS pages are loaded with $wgUseGlobalSiteCssJs = false' => [
 				'script',
 				[ 'UseGlobalSiteCssJs' => false ],
-				[],
 				'skinname',
-				'No JS pages are loaded with $wgUseGlobalSiteCssJs = false'
+				[],
 			],
-			[
+			'JS pages are loaded if $wgUseSiteCss = false' => [
 				'script',
 				[ 'UseSiteCss' => false ],
-				[
-					'MediaWiki:Global.js', 'MediaWiki:Global-skinname.js',
-				],
 				'skinname',
-				'JS pages are loaded if $wgUseSiteCss = false'
+				[
+					'MediaWiki:Global.js',
+					'MediaWiki:Global-skinname.js',
+				],
 			],
-			[
+			'CSS pages are loaded if $wgUseSiteJs = false' => [
 				'style',
 				[ 'UseSiteJs' => false ],
-				[
-					'MediaWiki:Global.css', 'MediaWiki:Global-skinname.css',
-				],
 				'skinname',
-				'CSS pages are loaded if $wgUseSiteJs = false'
+				[
+					'MediaWiki:Global.css',
+					'MediaWiki:Global-skinname.css',
+				],
 			],
-			[
+			'No CSS pages loaded if $wgUseSiteJs and $wgUseSiteCss are false' => [
 				'style',
 				[ 'UseSiteJs' => false, 'UseSiteCss' => false ],
-				[],
 				'skinname',
-				'No CSS pages loaded if $wgUseSiteJs and $wgUseSiteCss are false'
+				[],
 			],
-			[
+			'No JS pages loaded if $wgUseSiteJs and $wgUseSiteCss are false' => [
 				'script',
 				[ 'UseSiteJs' => false, 'UseSiteCss' => false ],
-				[],
 				'skinname',
-				'No JS pages loaded if $wgUseSiteJs and $wgUseSiteCss are false'
+				[],
 			],
-			[
+			'Global-monobook.css pages are loaded if monobook is set as the skin' => [
 				'style',
 				[],
-				[
-					'MediaWiki:Global.css', 'MediaWiki:Global-monobook.css'
-				],
 				'monobook',
-				'Global-monobook.css pages are loaded if monobook is set as the skin'
+				[
+					'MediaWiki:Global.css',
+					'MediaWiki:Global-monobook.css',
+				],
 			],
-			[
+			'Global-monobook.js pages are loaded if monobook is set as the skin' => [
 				'script',
 				[],
-				[
-					'MediaWiki:Global.js', 'MediaWiki:Global-monobook.js'
-				],
 				'monobook',
-				'Global-monobook.js pages are loaded if monobook is set as the skin'
+				[
+					'MediaWiki:Global.js',
+					'MediaWiki:Global-monobook.js',
+				],
 			],
 		];
 	}
 
 	/**
-	 * @covers \MediaWiki\GlobalCssJs\ResourceLoaderGlobalSiteModule::getPages
 	 * @dataProvider provideGetPages
-	 * @param $type
-	 * @param $configOverrides
-	 * @param $expectedPages
-	 * @param $skin
-	 * @param $desc
 	 */
-	public function testGetPages( $type, $configOverrides, $expectedPages, $skin, $desc ) {
+	public function testGetPages(
+		$type,
+		array $configOverrides,
+		$skin,
+		array $expectedPages
+	) {
 		$module = new ResourceLoaderGlobalSiteModule(
 			[ 'type' => $type ] + $this->getFakeOptions()
 		);
@@ -121,6 +119,6 @@ class ResourceLoaderGlobalSiteModuleTest extends \PHPUnit\Framework\TestCase {
 		$getPages->setAccessible( true );
 		$out = $getPages->invoke( $module, $context );
 		$pages = array_keys( $out );
-		$this->assertEquals( $expectedPages, $pages, $desc );
+		$this->assertEquals( $expectedPages, $pages, 'page names' );
 	}
 }
