68b7d41d2abee657bb313e8d2bd2a7e36cd5a9b6
Jforrester <jforrester@wikimedia.org>
2026-05-05 17:49:43 +0000

    Revert "Replace only use of main context"
    This reverts commit fad690b92a03e9062c29f2259777e60c9475d4c9.
    
    Bug: T425431
    Change-Id: Id2530152c6131938853e8360d377f54d07004d82

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -22,6 +22,7 @@
 namespace MediaWiki\GlobalCssJs;
 
 use MediaWiki\Config\Config;
+use MediaWiki\Context\RequestContext;
 use MediaWiki\EditPage\EditPage;
 use MediaWiki\Hook\EditPage__showEditForm_initialHook;
 use MediaWiki\Linker\Linker;
@@ -238,9 +239,9 @@ class Hooks implements
 	 * @param array &$prefs
 	 */
 	public function onGetPreferences( $user, &$prefs ) {
-		$config = MediaWikiServices::getInstance()->getMainConfig();
-		$allowUserCss = $config->get( 'AllowUserCss' );
-		$allowUserJs = $config->get( 'AllowUserJs' );
+		$ctx = RequestContext::getMain();
+		$allowUserCss = $ctx->getConfig()->get( 'AllowUserCss' );
+		$allowUserJs = $ctx->getConfig()->get( 'AllowUserJs' );
 
 		if ( !$allowUserCss && !$allowUserJs ) {
 			// No user CSS or JS allowed
@@ -273,9 +274,7 @@ class Hooks implements
 			[ 'globalcssjs' => [
 				'type' => 'info',
 				'raw' => 'true',
-				'default' => static function ( $info ) {
-					return $info['parent']->getContext()->getLanguage()->pipeList( $linkTools );
-				},
+				'default' => $ctx->getLanguage()->pipeList( $linkTools ),
 				'label-message' => 'globalcssjs-custom-css-js',
 				'section' => 'rendering/skin',
 			] ],
